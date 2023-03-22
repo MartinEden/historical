3bd34b01d78e28e4f3e879c6d1f560c3443f0f21
Martin Eden <martin.s.eden@gmail.com>
2023-03-22 13:49:53 +0000

    Add US states; start committing some output

--- src/main/kotlin/eden/historical/storage/HumanReadableStore.kt
+++ src/main/kotlin/eden/historical/storage/HumanReadableStore.kt
@@ -4,14 +4,11 @@ import eden.historical.Settings
 import eden.historical.models.CategorizedBook
 import java.io.File
 import java.nio.file.Paths
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 import kotlin.io.path.absolutePathString
 
 class HumanReadableStore : Store {
     private val books = mutableListOf<CategorizedBook>()
-    private val nowAsString = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH.mm.ss"))
-    private val outputFile = File(Paths.get(Settings.outputPath,"$nowAsString.txt").absolutePathString())
+    private val outputFile = File(Paths.get(Settings.outputPath,"output.txt").absolutePathString())
 
     override fun put(book: CategorizedBook) {
         books.add(book)
