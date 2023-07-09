6b65085eb7c366d50509395ced195ae0a76cf952
Martin Eden <martin.s.eden@gmail.com>
2023-07-09 20:01:48 +0100

    Match centuries without the definite article

--- src/main/kotlin/eden/historical/categorization/CenturyRule.kt
+++ src/main/kotlin/eden/historical/categorization/CenturyRule.kt
@@ -5,7 +5,7 @@ import eden.historical.models.Century
 import eden.historical.models.Period
 
 class CenturyRule(private val century: Century) : Rule {
-    private val regex = Regex("""the ${century.ordinalAsWord}( |-)century""")
+    private val regex = Regex("""${century.ordinalAsWord}( |-)century""")
     private val nextCenturyRegex = makeNextCenturyRegex()
 
     override fun apply(book: BookMetadata): Categorization? {
@@ -27,7 +27,7 @@ class CenturyRule(private val century: Century) : Rule {
     private fun makeNextCenturyRegex(): Regex? {
         val nextCentury = century.next
         return if (nextCentury != null) {
-            Regex("""the ${century.ordinalAsNumber} and ${nextCentury.ordinalAsNumber} centuries""")
+            Regex("""${century.ordinalAsNumber} and ${nextCentury.ordinalAsNumber} centuries""")
         } else {
             null
         }
--- src/main/kotlin/eden/historical/categorization/YearRules.kt
+++ src/main/kotlin/eden/historical/categorization/YearRules.kt
@@ -13,9 +13,11 @@ object TheYearIsRule : SynopsisRegexRule(Regex("""(?:the\s+year\s+is|it\s+is)\s+
 object AnyYearRule : SynopsisRegexRule(Regex("""\W(\d{4})\W""")) {
     override fun handleMatch(match: MatchResult, book: BookMetadata): Categorization? {
         val year = match.groups[1]!!.value.toInt()
-        val winnerRegex = Regex("""winner of [^.]*$year [^.]*prize""")
-        return if (winnerRegex in book.synopsis) {
-            println("Skipping year because it is describing the year the book won a prize")
+        val redHerringRegexes = listOf(
+            Regex("""winner of [^.]*$year [^.]*prize"""),
+            Regex("""written in $year"""),
+        )
+        return if (redHerringRegexes.any { it in book.synopsis }) {
             null
         } else {
             return Categorization(Period.Range(year.toString(), year, year) withConfidence 0.25f)
@@ -23,7 +25,7 @@ object AnyYearRule : SynopsisRegexRule(Regex("""\W(\d{4})\W""")) {
     }
 }
 
-// TODO: Currently we treat centuries (e.g. 1900s as if they were just the first decade
+// TODO: Currently we treat centuries (e.g. 1900s) as if they were just the first decade
 object DecadeRule : SynopsisRegexRule(Regex("""(?<phrasing>in the (?:early|late)?)?\s*(?<decade>\d{4})s""")) {
     override fun handleMatch(match: MatchResult, book: BookMetadata): Categorization? {
         val decade = match.groups["decade"]!!.value.toInt()
