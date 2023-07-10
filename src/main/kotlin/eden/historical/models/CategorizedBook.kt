54b7e9b59210986ffeea65d5245adcce36959254
Martin Eden <martin.s.eden@gmail.com>
2023-07-10 16:41:59 +0100

    Add a TODO

--- src/main/kotlin/eden/historical/models/CategorizedBook.kt
+++ src/main/kotlin/eden/historical/models/CategorizedBook.kt
@@ -3,7 +3,7 @@ package eden.historical.models
 data class CategorizedBook(
     val book: Book,
     val period: Period,
-    val place: Place
+    val place: Place    // TODO: This should be a collection of Places
 ) {
     val title
         get() = book.title
